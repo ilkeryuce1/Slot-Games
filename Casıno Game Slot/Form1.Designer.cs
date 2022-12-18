e3420ad3fa3dd2cc45b12e89e95a3c3826965f22
ilker yüce <109898238+ilkeryuce1@users.noreply.github.com>
2022-12-18 21:34:09 +0300

    Add files via upload

---
+++ Casıno Game Slot/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Casıno_Game_Slot
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_balance = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(476, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 54);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Let\'s Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(488, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Balance";
+            // 
+            // lbl_balance
+            // 
+            this.lbl_balance.AutoSize = true;
+            this.lbl_balance.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_balance.Location = new System.Drawing.Point(551, 50);
+            this.lbl_balance.Name = "lbl_balance";
+            this.lbl_balance.Size = new System.Drawing.Size(14, 16);
+            this.lbl_balance.TabIndex = 5;
+            this.lbl_balance.Text = "0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Casıno_Game_Slot.Properties.Resources.mango;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 109);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 110);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Casıno_Game_Slot.Properties.Resources.mango;
+            this.pictureBox2.Location = new System.Drawing.Point(153, 109);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(132, 110);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pictureBox3.Image = global::Casıno_Game_Slot.Properties.Resources.mango;
+            this.pictureBox3.Location = new System.Drawing.Point(291, 109);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(132, 110);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(15, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(283, 29);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Welcome To Slot Game";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox1.Location = new System.Drawing.Point(476, 131);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 36);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(471, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Price";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(15, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(408, 46);
+            this.label5.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Casıno_Game_Slot.Properties.Resources.Jackpot_Slots_;
+            this.ClientSize = new System.Drawing.Size(645, 315);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lbl_balance);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slot Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_balance;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Casıno Game Slot/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Casıno_Game_Slot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random odun = new Random();
+        int r1, r2, r3, move;
+        int a;
+        int b;
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            b++;//5. oynamada kazandırmak için
+            if (textBox1.Text == null || textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("You Can't Play Games Without Deposit");
+            }
+            else
+            {
+                a = Convert.ToInt32(lbl_balance.Text);
+
+                a += int.Parse(textBox1.Text);
+                lbl_balance.Text = a.ToString();
+                textBox1.Clear();
+                if (int.Parse(lbl_balance.Text) > 0)
+                {
+                    string konum = Application.StartupPath + "\\song.wav";
+                    gazıno.SoundLocation = konum;
+                    gazıno.Play();
+                    timer1.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Yetersiz Bakiye");
+                }
+            }
+
+
+
+
+
+
+
+
+        }
+        void Kazanıldı()
+        {
+            //if (pictureBox1 == pictureBox2 || pictureBox2 == pictureBox3 || pictureBox3 == pictureBox1)
+            //{
+            //    MessageBox.Show("Kazandınız");
+            //}
+
+            if (r1 == r2 && r3 == r2)
+            {
+                a = a * 2;
+               
+                label5.Text=(a.ToString() + " TL You Win");
+                lbl_balance.Text = "0";
+
+
+            }
+            else
+            {
+                label5.Text =("You Lost");
+                a -= a;
+            }
+            lbl_balance.Text = a.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label3.BackColor = Color.Transparent;
+        }
+
+
+
+        SoundPlayer gazıno = new SoundPlayer();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            move++;
+
+            if (move < 30)
+            {
+                if (b == 5 && move == 29)
+                {
+                    r1 = odun.Next(0, 1);
+                    r2 = odun.Next(0, 1);
+                    r3 = odun.Next(0, 1);
+                }
+                else
+                {
+                    r1 = odun.Next(0, 3);
+                    r2 = odun.Next(0, 3);
+                    r3 = odun.Next(0, 3);
+                }
+
+
+                switch (r1)
+                {
+                    case 0:
+                        pictureBox1.Image = Properties.Resources.mango;
+                        break;
+                    case 1:
+                        pictureBox1.Image = Properties.Resources.artfruit_banner_incir_00_1;
+                        break;
+                    case 2:
+                        pictureBox1.Image = Properties.Resources.ejder;
+                        break;
+                }
+                switch (r2)
+                {
+                    case 0:
+                        pictureBox2.Image = Properties.Resources.mango;
+                        break;
+                    case 1:
+                        pictureBox2.Image = Properties.Resources.artfruit_banner_incir_00_1;
+                        break;
+                    case 2:
+                        pictureBox2.Image = Properties.Resources.ejder;
+                        break;
+                }
+                switch (r3)
+                {
+                    case 0:
+                        pictureBox3.Image = Properties.Resources.mango;
+                        break;
+                    case 1:
+                        pictureBox3.Image = Properties.Resources.artfruit_banner_incir_00_1;
+                        break;
+                    case 2:
+                        pictureBox3.Image = Properties.Resources.ejder;
+                        break;
+                }
+
+
+            }
+            else
+            {
+                gazıno.Stop();
+                timer1.Stop();
+                move = 0;
+                Kazanıldı();
+            }
+
+        }
+    }
+}
